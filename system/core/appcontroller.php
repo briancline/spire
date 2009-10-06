bcf6fd3fa77601e2ef9d873147a386d6d4e840be
Brian Cline <brian.cline@gmail.com>
2009-10-06 16:34:20 -0500

    Add a very simple, less ugly and less cumbersome method of obtaining POST values.

--- system/core/appcontroller.php
+++ system/core/appcontroller.php
@@ -2,9 +2,17 @@
 
 	class AppController extends Controller
 	{
+		protected $postVars = false;
+		
+		
 		function __construct()
 		{
 			parent::__construct();
+			
+			$post = new stdClass();
+			foreach($_POST as $postKey => $postValue) {
+				$this->postVars->$postKey = $postValue;
+			}
 		}
 		
 		function __destruct()
